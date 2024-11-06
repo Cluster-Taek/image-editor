284b0bb3582e442a34293a594ca46acbc4545d90
Cluster-Taek <yst811@naver.com>
2024-11-06 10:40:09 +0900

    initial commit

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import { css } from '@/styled-system/css';
+
+export default function Home() {
+  return (
+    <div>
+      <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>Hello 🐼!</div>
+    </div>
+  );
+}
---
+++ src/contexts/core-provider.tsx
@@ -0,0 +1,11 @@
+import SessionProvider from '@/contexts/session-provider';
+
+interface ICoreProviderProps {
+  children?: React.ReactNode;
+}
+
+const CoreProvider = ({ children }: ICoreProviderProps) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default CoreProvider;
